88a5d8f0893693a6bde768a101b84b8f36d7d01b
dirtdorks <therosemarygarden@gmail.com>
2026-03-02 15:09:33 -0800

    Refactor map initialization and GPX trail settings
    Updated the base map layer and adjusted GPX trail options.

--- script.js
+++ script.js
@@ -5,19 +5,29 @@ const map = L.map('map', {
 
 // Lighter Topographic Base Map
 L.tileLayer(
-  'https://{s}.tile.opentopomap.org/{z}/{x}/{y}.png',
-  {
-    attribution: '&copy; OpenStreetMap contributors'
-  }
+'https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png',
+{
+attribution: '&copy; OpenStreetMap',
+opacity: 0.65
+}
 ).addTo(map);
 
 // Forest Park GPX Trails - Lichen Green
-new L.GPX('trails/forest-park-trails.gpx', {
-  async: true,
-  polyline_options: {
-    color: '#8FAF7B',   // lichen green
-    weight: 4,
-    opacity: 0.95
+polyline_options: {
+color: '#b6f2c2', // brighter forest green
+weight: 4, // thicker = emphasis
+opacity: 1,
+lineCap: 'round'
+}new L.GPX('trails/forest-park-trails.gpx', {
+async: true,
+polyline_options: {
+color: '#4c8f6b',
+weight: 8,
+opacity: 0.25,
+lineCap: 'round'
+}
+}).addTo(map);
+
   },
   marker_options: {
     startIconUrl: null,
@@ -26,4 +36,5 @@ new L.GPX('trails/forest-park-trails.gpx', {
   }
 }).on('loaded', e => {
   map.fitBounds(e.target.getBounds());
+
 }).addTo(map);
